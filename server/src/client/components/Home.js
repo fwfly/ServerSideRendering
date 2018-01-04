7b2926681bc2fa963eaaf1a70172efd1f63bf478
fwfly <fawafwfly@gmail.com>
2018-01-04 18:11:27 +1100

    30-Routing Tiers

---
+++ server/src/client/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const Home = () =>{
+  return (
+    <div>
+      <div>Hi There !!!</div>
+      <button onClick={ ()=>{ console.log("click")  }  }>Press</button>
+    </div>
+  );
+}
+
+export default Home
