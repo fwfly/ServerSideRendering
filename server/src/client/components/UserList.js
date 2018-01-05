43eff5e2c4b1ba7047286011586b762864ac4a63
fwfly <fawafwfly@gmail.com>
2018-01-05 14:54:01 +1100

    38_UsersList Component

---
+++ server/src/client/components/UserList.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {fetchUsers} from '../actions';
+
+
+class UserList extends Component{
+  componentDidMount(){
+    this.props.fetchUsers();
+  }
+
+  renderUser(){
+    this.props.users.map( user =>{
+      return(
+        <li key={user.id}>
+          {user.name}
+        </li>
+      );
+    })
+  }
+
+  render(){
+    return(
+      <div>
+        Here's big list of users:
+        <ul>
+          {this.renderUser()}
+        </ul>
+      </div>
+    );
+  }
+}
+
+function mapStateToPorps(state){
+ return {users : state.users};
+}
+
+export default connect(mapStateToPorps, {fetchUsers})(UserList);
