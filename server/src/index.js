baa467417fa48ccefb0da9066a0e2910683f3c12
fwfly <fawafwfly@gmail.com>
2018-01-08 10:15:17 +1100

    45 The MatchRoutes Function

--- server/src/index.js
+++ server/src/index.js
@@ -2,12 +2,17 @@ import 'babel-polyfill';
 import express from 'express';
 import renderer from './helper/renderer';
 import createStore from './helper/createStore';
+import { matchRoutes } from 'react-router-config';
+import Routes from './client/Routes';
 
 const app = express();
 app.use(express.static('public'));
 app.get('*', (req, res)=>{
 
   const store = createStore();
+
+  matchRoutes(Routes, req.path);
+
   res.send(renderer(req, store));
 })
 
