be1ecbfeaea20f6f315799b9e27649b4e8d2357b
fwfly <fawafwfly@gmail.com>
2018-01-09 16:39:01 +1100

    66 Adding an App Component

--- server/src/client/Routes.js
+++ server/src/client/Routes.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import HomePage from './pages/HomePage';
 import UserListPage from './pages/UserListPage';
+import App from './App';
 
-export default[
-  {
-    ...HomePage,
-    path : "/",
-    exact :true
-  },{
-    ...UserListPage,
-    path : "/users",
-  }
-];
+export default[{
+  ...App,
+  routes:[
+    {
+      ...HomePage,
+      path : "/",
+      exact :true
+    },{
+      ...UserListPage,
+      path : "/users",
+    }
+  ]
+}];
