40e33622e54047eba0ea0ab74b2119ca30939f9d
fwfly <fawafwfly@gmail.com>
2018-01-09 14:38:25 +1100

    60. Proxy Setup

--- server/src/index.js
+++ server/src/index.js
@@ -2,10 +2,17 @@ import 'babel-polyfill';
 import express from 'express';
 import renderer from './helper/renderer';
 import createStore from './helper/createStore';
+import proxy from 'express-http-proxy';
 import { matchRoutes } from 'react-router-config';
 import Routes from './client/Routes';
 
 const app = express();
+app.use('/api', proxy('http://react-ssr-api.herokuapp.com',{
+  proxyReqOptDecorator(opts){
+    opts.header['x-forwarded-host'] = 'localhost:3000';
+    return opts;
+  }
+}));
 app.use(express.static('public'));
 app.get('*', (req, res)=>{
 
