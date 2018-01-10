3d67d2f00ce5dce2181c03aded399f9a77c0bf36
fwfly <fawafwfly@gmail.com>
2018-01-10 11:36:21 +1100

    70 Connecting the Header

--- server/src/client/components/Header.js
+++ server/src/client/components/Header.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import {connect} from 'react-redux';
 
-export default () => {
+const Header = ({auth}) => {
+  console.log("My status is ", auth);
+
   return (
     <div>
       <Link to="/">React SSR</Link>
     </div>
   );
 };
+
+
+function mapStatsToProps({auth}){
+    return {auth}
+}
+
+export default connect(mapStatsToProps)(Header);
